9e6b5a496acdd5b2ea807810a4460015369e8de8
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-22 21:31:56 -0300

    test: should return 200 on GET /kiosks

--- packages/backend/tests/kiosk.test.ts
+++ packages/backend/tests/kiosk.test.ts
@@ -1,36 +1,9 @@
-import KioskModel from "../src/models/Kiosk.model";
-import { connectToDatabase, dropCollections, dropDatabase } from "./db";
-import dayjs from "dayjs";
+import request from "supertest";
+import app from "../src/app";
 
-beforeAll(async () => {
-  await connectToDatabase();
-});
-
-afterAll(async () => {
-  await dropDatabase();
-});
-
-afterEach(async () => {
-  await dropCollections();
-});
-
-describe("Kiosk model testing", () => {
-  it("should save kiosk correctly on mongodb", async () => {
-    const storeClosesAt = dayjs().add(3, "hours").toDate();
-    const storeOpensAt = dayjs().toDate();
-
-    const validKiosk = await KioskModel.create({
-      description: "This is the philosophers kiosk.",
-      isKioskClosed: false,
-      serialKey: "this-is-my-serial-key",
-      storeClosesAt,
-      storeOpensAt,
-    });
-
-    expect(validKiosk.serialKey).toBe("this-is-my-serial-key");
-    expect(validKiosk.isKioskClosed).toBe(false);
-    expect(validKiosk.description).toBe("This is the philosophers kiosk.");
-    expect(validKiosk.storeClosesAt).toBe(storeClosesAt);
-    expect(validKiosk.storeOpensAt).toBe(storeOpensAt);
+describe("GET /kiosks", () => {
+  it("should return 200 on GET /kiosks", async () => {
+    const response = await request(app).get("/kiosks");
+    expect(response.status).toBe(200);
   });
 });
