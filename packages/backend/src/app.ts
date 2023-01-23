5c0e76d8b5a4ad86057287ae1f61f8e8959e7289
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-23 00:42:47 -0300

    feat: use json body parser

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -4,6 +4,8 @@ import kiosksRoutes from "./routes/kiosks.routes";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
