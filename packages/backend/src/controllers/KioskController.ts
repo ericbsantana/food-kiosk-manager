e8a41531d2ea358e07d9025e17cd36bc6aa089ef
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-23 02:37:10 -0300

    refactor: move functions inside routes to controllers folder

---
+++ packages/backend/src/controllers/KioskController.ts
@@ -0,0 +1,45 @@
+import { validationResult } from "express-validator";
+import KioskService from "../services/kiosk.service";
+import { Request, Response } from "express";
+import IException from "../interfaces/exception";
+
+const KioskController = {
+  POST: async (req: Request, res: Response) => {
+    const validation = validationResult(req);
+
+    if (!validation.isEmpty()) {
+      return res.status(400).send({ errors: validation.mapped() });
+    }
+
+    try {
+      const createdKiosk = await KioskService.createOne(req.body);
+
+      return res
+        .status(200)
+        .send({ message: "Kiosk created successfully", data: createdKiosk });
+    } catch (err) {
+      return res.sendStatus(502);
+    }
+  },
+  GET_BY_ID: async (req: Request, res: Response) => {
+    const id = req.params.id;
+
+    try {
+      const kiosk = await KioskService.findById(id);
+      return res.status(200).send(kiosk);
+    } catch (err: any) {
+      const error: IException = err;
+      return res.status(error.status).send({ message: error.message });
+    }
+  },
+  GET: async (req: Request, res: Response) => {
+    try {
+      const kiosks = await KioskService.find({});
+      return res.status(200).send(kiosks);
+    } catch (err) {
+      return res.sendStatus(502);
+    }
+  },
+};
+
+export default KioskController;
--- packages/backend/src/routes/kiosks.routes.ts
+++ packages/backend/src/routes/kiosks.routes.ts
@@ -1,52 +1,11 @@
 import express, { Request, Response } from "express";
-import KioskService from "../services/kiosk.service";
-import IException from "../interfaces/exception";
-import { validationResult } from "express-validator";
 import KioskValidationMiddleware from "../middlewares/KioskValidationMiddleware";
+import KioskController from "../controllers/KioskController";
 
 const router = express.Router();
 
-router.get("/kiosks", async (req: Request, res: Response) => {
-  try {
-    const kiosks = await KioskService.find({});
-    return res.status(200).send(kiosks);
-  } catch (err) {
-    return res.sendStatus(502);
-  }
-});
-
-router.get("/kiosks/:id", async (req: Request, res: Response) => {
-  const id = req.params.id;
-
-  try {
-    const kiosk = await KioskService.findById(id);
-    return res.status(200).send(kiosk);
-  } catch (err: any) {
-    const error: IException = err;
-    return res.status(error.status).send({ message: error.message });
-  }
-});
-
-router.post(
-  "/kiosks",
-  KioskValidationMiddleware,
-  async (req: Request, res: Response) => {
-    const validation = validationResult(req);
-
-    if (!validation.isEmpty()) {
-      return res.status(400).send({ errors: validation.mapped() });
-    }
-
-    try {
-      const createdKiosk = await KioskService.createOne(req.body);
-
-      return res
-        .status(200)
-        .send({ message: "Kiosk created successfully", data: createdKiosk });
-    } catch (err) {
-      return res.sendStatus(502);
-    }
-  }
-);
+router.get("/kiosks", KioskController.GET);
+router.get("/kiosks/:id", KioskController.GET_BY_ID);
+router.post("/kiosks", KioskValidationMiddleware, KioskController.POST);
 
 export default router;
