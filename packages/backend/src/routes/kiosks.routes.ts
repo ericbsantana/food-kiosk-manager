a76f8990e8815650452031fe7018c76589c882cf
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-23 06:25:56 -0300

    feat: add ObjectIdValidationMiddleware to DELETE and GET /kiosks/:id

--- packages/backend/src/routes/kiosks.routes.ts
+++ packages/backend/src/routes/kiosks.routes.ts
@@ -1,12 +1,21 @@
 import express, { Request, Response } from "express";
 import KioskValidationMiddleware from "../middlewares/KioskValidationMiddleware";
 import KioskController from "../controllers/KioskController";
+import ObjectIdValidationMiddleware from "../middlewares/ObjectIdValidationMiddleware";
 
 const router = express.Router();
 
 router.get("/kiosks", KioskController.GET);
-router.get("/kiosks/:id", KioskController.GET_BY_ID);
+router.get(
+  "/kiosks/:id",
+  ObjectIdValidationMiddleware,
+  KioskController.GET_BY_ID
+);
 router.post("/kiosks", KioskValidationMiddleware, KioskController.POST);
-router.delete("/kiosks/:id", KioskController.DELETE);
+router.delete(
+  "/kiosks/:id",
+  ObjectIdValidationMiddleware,
+  KioskController.DELETE
+);
 
 export default router;
