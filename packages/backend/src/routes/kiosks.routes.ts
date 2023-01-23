f9b7aef6eae2eba14b106511b2ada255d434095c
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-23 00:43:37 -0300

    refactor: return all res.status/res.send on kiosks routes

--- packages/backend/src/routes/kiosks.routes.ts
+++ packages/backend/src/routes/kiosks.routes.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/kiosks", async (req: Request, res: Response) => {
   const kiosks = await KioskService.find({});
-  res.status(200).send(kiosks);
+  return res.status(200).send(kiosks);
 });
 
 router.get("/kiosks/:id", async (req: Request, res: Response) => {
@@ -15,10 +15,10 @@ router.get("/kiosks/:id", async (req: Request, res: Response) => {
 
   try {
     const kiosk = await KioskService.findById(id);
-    res.status(200).send(kiosk);
+    return res.status(200).send(kiosk);
   } catch (err: any) {
     const error: IException = err;
-    res.status(error.status).send({ message: error.message });
+    return res.status(error.status).send({ message: error.message });
   }
 });
 
