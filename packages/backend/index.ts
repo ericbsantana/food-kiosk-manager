3f923ad2ddc0bcb86ee8c3cbaaf724dbef479830
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-22 21:14:07 -0300

    chore: install dayjs, mongodb-memory-server, nodemon, ts-node

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,4 +1,11 @@
 import app from "./app";
+import mongoose from "mongoose";
+import config from "config";
+
 const port = process.env.PORT || 3000;
+
+const databaseUrl: string = config.get("database.url");
+
+mongoose.connect(databaseUrl);
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
