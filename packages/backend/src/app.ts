2e5246b125a01d4f4b496f0ad80c8c3c1c5b55f0
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-22 21:44:33 -0300

    feat: return kiosks on GET /kiosks

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Express } from "express";
 import express from "express";
+import KioskModel from "./models/Kiosk.model";
 
 const app = express();
 
@@ -7,8 +8,9 @@ app.get("/", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
-app.get("/kiosks", (req: Request, res: Response) => {
-  res.sendStatus(200);
+app.get("/kiosks", async (req: Request, res: Response) => {
+  const kiosks = await KioskModel.find({});
+  res.status(200).send(kiosks);
 });
 
 export default app;
