18fb46b686944ada0d7ce4fefcb8d39cfcb98b70
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-25 23:05:20 -0300

    feat: creaate job to run every 60 seconds to close and update kiosks status

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,6 +1,8 @@
 import app from "./src/app";
 import mongoose from "mongoose";
 import config from "config";
+import KioskService from "./src/services/kiosk.service";
+import { ToadScheduler, SimpleIntervalJob, AsyncTask } from "toad-scheduler";
 
 const port = process.env.PORT || 3000;
 
@@ -8,4 +10,24 @@ const databaseUrl: string = config.get("database.url");
 
 mongoose.connect(databaseUrl);
 
+const scheduler = new ToadScheduler();
+
+scheduler.addSimpleIntervalJob(
+  new SimpleIntervalJob(
+    { seconds: 60 },
+    new AsyncTask(
+      "Close and open kiosks accordingly to its close and open time",
+      () => {
+        return KioskService.updateAllKiosksStatus().then((result) => {
+          console.log(
+            `Closed ${result.numberOfKiosksClosed} and opened ${result.numberOfKiosksOpened} kiosks.`
+          );
+        });
+      },
+      (err: Error) => {
+        console.error("Failed to update kiosks.");
+      }
+    )
+  )
+);
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
