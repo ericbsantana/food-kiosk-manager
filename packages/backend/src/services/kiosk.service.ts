7d8e54ca709622f9fc0a9f2fd5e11f733976a328
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-25 23:05:01 -0300

    feat: create updateAllKiosksStatus service

--- packages/backend/src/services/kiosk.service.ts
+++ packages/backend/src/services/kiosk.service.ts
@@ -2,6 +2,8 @@ import mongoose, { FilterQuery } from "mongoose";
 import KioskModel from "../models/Kiosk.model";
 import IKiosk from "../interfaces/kiosk";
 import Exception from "../errors/Exception";
+import dayjs from "dayjs";
+import BluebirdPromise from "bluebird";
 
 const find = async (params: FilterQuery<IKiosk>) => {
   return KioskModel.find(params);
@@ -18,6 +20,52 @@ const deleteById = async (id: string) => KioskModel.findByIdAndDelete(id);
 const updateById = async (id: string, body: any) =>
   KioskModel.findByIdAndUpdate(id, body, { new: true });
 
-const KioskService = { findById, find, createOne, deleteById, updateById };
+const updateAllKiosksStatus = async () => {
+  const now = dayjs().toISOString();
+
+  const toBeOpened = await KioskModel.find({
+    storeOpensAt: { $lte: now },
+    storeClosesAt: { $gte: now },
+    isKioskClosed: true,
+  });
+
+  const opened = await BluebirdPromise.map(toBeOpened, (closedKiosks) =>
+    KioskModel.findByIdAndUpdate(
+      closedKiosks._id,
+      { isKioskClosed: false },
+      { new: true }
+    )
+  );
+
+  const toBeClosed = await KioskModel.find({
+    $or: [
+      { storeOpensAt: { $gte: now }, storeClosesAt: { $gte: now } },
+      { storeOpensAt: { $lte: now }, storeClosesAt: { $lte: now } },
+    ],
+    isKioskClosed: false,
+  });
+
+  const closed = await BluebirdPromise.map(toBeClosed, (closedKiosks) =>
+    KioskModel.findByIdAndUpdate(
+      closedKiosks._id,
+      { isKioskClosed: true },
+      { new: true }
+    )
+  );
+
+  return {
+    numberOfKiosksOpened: opened.length,
+    numberOfKiosksClosed: closed.length,
+  };
+};
+
+const KioskService = {
+  findById,
+  find,
+  createOne,
+  deleteById,
+  updateById,
+  updateAllKiosksStatus,
+};
 
 export default KioskService;
