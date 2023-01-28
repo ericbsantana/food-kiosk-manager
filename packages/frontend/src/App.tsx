278326bace5f75e5aa80275f29f21859019c0f2e
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-28 02:35:27 -0300

    feat: create index page title

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="h-screen w-full bg-neutral-900 text-white p-10">
+      <div className="text-center space-y-1">
+        <h1 className="text-4xl">Welcome to our</h1>
+        <h1 className="text-4xl font-bold">Kiosk Management</h1>
+      </div>
+    </div>
+  );
 };
 
 export default App;
