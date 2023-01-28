16f0d82d4c9b7f4df9e46e9d95f6788242ce5291
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-28 02:40:12 -0300

    feat: enable cors on api

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Express } from "express";
 import express from "express";
 import kiosksRoutes from "./routes/kiosks.routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.sendStatus(200);
