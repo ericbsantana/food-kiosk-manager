2e475501de612cbb3f751a03a59a8b8b1bbe4d84
ericbsantana <bitencourteric@hotmail.com>
2023-01-29 17:23:50 -0300

    feat: add spacing on main app function

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -16,7 +16,7 @@ const App = () => {
   }
 
   return (
-    <div className="h-screen w-full bg-neutral-900 text-white p-10">
+    <div className="h-screen w-full bg-neutral-900 text-white p-10 space-y-5">
       <div className="text-center space-y-1">
         <h1 className="text-4xl">Welcome to our</h1>
         <h1 className="text-4xl font-bold">Kiosk Management</h1>
