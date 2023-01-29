4b25610cd8aa32f1a74708a2d94352ab047bce18
ericbsantana <bitencourteric@hotmail.com>
2023-01-29 18:03:11 -0300

    refactor: move App to Home page

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
   },
   {
     path: "/create",
---
+++ packages/frontend/src/pages/Home.tsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import useSWR from "swr";
+import Table from "../components/Table";
+import { PlusIcon } from "@heroicons/react/24/solid";
+import { Link } from "react-router-dom";
+
+const fetcher = (endpoint: string) =>
+  axios.get(`http://localhost:3001${endpoint}`).then((res) => res.data);
+
+const App = () => {
+  const { data = [], isLoading, error } = useSWR("/kiosks", fetcher);
+
+  if (isLoading) {
+    return null;
+  }
+
+  return (
+    <div className="h-screen w-full bg-neutral-900 text-white p-10 space-y-5">
+      <div className="text-center space-y-5">
+        <div>
+          <h1 className="text-4xl">Welcome to our</h1>
+          <h1 className="text-4xl font-bold">Kiosk Management</h1>
+        </div>
+        <div>
+          <Link
+            type="button"
+            to="/create"
+            className="text-white bg-neutral-600 hover:bg-neutral-700 focus:ring-2 focus:outline-none focus:ring-neutral-500 font-medium rounded-md text-sm px-5 py-2.5 text-center inline-flex items-center mr-2"
+          >
+            Create new kiosk
+          </Link>
+        </div>
+      </div>
+      <div>
+        <Table data={data} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
