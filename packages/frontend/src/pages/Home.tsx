262a8f2b5ab7b7cbe80e5f24e0ca6d2e193a13f9
ericbsantana <bitencourteric@hotmail.com>
2023-01-29 22:18:10 -0300

    feat: show Spinner when loading kiosks

--- packages/frontend/src/pages/Home.tsx
+++ packages/frontend/src/pages/Home.tsx
@@ -1,18 +1,43 @@
-import axios from "axios";
 import useSWR from "swr";
 import Table from "../components/Table";
 import { Link } from "react-router-dom";
 import { Fragment } from "react";
 import { fetcher } from "../helpers/fetcher";
 
-const fetcher = (endpoint: string) =>
-  axios.get(`http://localhost:3001${endpoint}`).then((res) => res.data);
+const Spinner = () => {
+  return (
+    <svg
+      className="animate-spin -ml-1 mr-3 h-10 w-10 text-white"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  );
+};
 
 const Home = () => {
   const { data = [], isLoading, error, mutate } = useSWR("/kiosks", fetcher);
 
   if (isLoading) {
-    return null;
+    return (
+      <div className="h-full flex justify-center items-center">
+        <Spinner />
+      </div>
+    );
   }
 
   if (error) {
