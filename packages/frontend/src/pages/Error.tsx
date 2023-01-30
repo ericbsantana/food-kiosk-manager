df0ee53534a5db9d2fd9dc57a63838b0420ed7b5
ericbsantana <bitencourteric@hotmail.com>
2023-01-29 21:44:06 -0300

    feat: add button to go back home on error page

--- packages/frontend/src/pages/Error.tsx
+++ packages/frontend/src/pages/Error.tsx
@@ -1,13 +1,25 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
   const error: any = useRouteError();
+  const navigate = useNavigate();
 
   return (
     <div className="h-full flex justify-center flex-col content-center text-center space-y-5">
       <h1 className="text-5xl font-bold">Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>{error.statusText}</p>
+      <div>
+        <button
+          type="button"
+          className="button"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Back to home
+        </button>
+      </div>
     </div>
   );
 };
